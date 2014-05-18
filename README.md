f9e2b36977198177d47b1600d145a95f9a2cd729
andrejm <andrej@plusko.net>
2014-05-18 09:47:52 +0200

    add tip to other useful repo, add new wp-config snippets

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ wp_goodies
 ==========
 
 Short WP snippets often used in projects
+
+Other, similar
+==============
+[https://github.com/senlin/Code-Snippets](https://github.com/senlin/Code-Snippets)
---
+++ wp-config.php
@@ -0,0 +1,10 @@
+<?php
+
+
+// disable editing of theme files from the WordPress Dashboard
+// source: https://github.com/senlin/Code-Snippets/blob/master/WP-CONFIG/disallow-file-edit.php
+define ( 'DISALLOW_FILE_EDIT', true );
+
+// DISABLE THEME/PLUGIN EDITOR
+// source: https://github.com/senlin/Code-Snippets/blob/master/WP-CONFIG/disable-theme-plugin-editor.php
+define('DISALLOW_FILE_EDIT',true);
