b36eab413909175aaef7d786cb5ab8085fd2cd39
Andrej <hello@andrzej.sk>
2019-01-27 10:18:23 +0100

    add wp config error logging setup for prod

--- wp-config.php
+++ wp-config.php
@@ -1,5 +1,10 @@
 <?php
 
+// debugging on production
+// see wp-content/debug.log
+define('WP_DEBUG', true);
+define('WP_DEBUG_LOG', true);
+define( 'WP_DEBUG_DISPLAY', false );
 
 // disable editing of theme files from the WordPress Dashboard
 // source: https://github.com/senlin/Code-Snippets/blob/master/WP-CONFIG/disallow-file-edit.php
